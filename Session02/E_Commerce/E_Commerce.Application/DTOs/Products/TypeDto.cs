7d8de1db25d29faa9b4f29fa3bea3432997c09bf
Abdelrahman_Ahmed <barmga72@gmail.com>
2026-06-28 07:24:54 +0300

    Add Session02 project

---
+++ Session02/E_Commerce/E_Commerce.Application/DTOs/Products/TypeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerce.Application.DTOs.Products;
+
+public class TypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+}
---
+++ Session02/E_Commerce/E_Commerce.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using E_Commerce.Domain.Common;
+
+namespace E_Commerce.Domain.Contracts;
+
+public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+{
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+    Task<TEntity?> GetByIdAsync(TKey id, CancellationToken ct = default);
+    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken ct = default);
+}
